adc369f163fea062156c211012699e10e41a1832
Sourav Giri <computerscienceengineer1@gmail.com>
2026-05-18 12:37:28 +0530

    hello

--- string part_2/main.js
+++ string part_2/main.js
@@ -1,1 +1,59 @@
-console.log("hello")
+/**
+ * checks two strings are anagram or not anagrams, words have the same length and same character count
+ */
+
+
+// let str1 = "anar";
+// let str2 = "naar";
+
+
+// function checkAnagram(str1,str2){
+//   if(str1.length!==str2.length) return false
+ 
+//   let charCount = {};
+
+//   for(let i=0;i<str1.length;i++){
+//     let ch = str1[i];
+//     charCount[ch] = (charCount[ch] || 0) +1
+//   }
+
+//   for(let i=0;i<str2.length;i++){
+//     let ch = str2[i];
+//     if(!charCount[ch]) return false;
+//     charCount[ch]--
+//   }
+
+//   return true
+
+// }
+
+
+
+
+
+/**
+ * maximum number of wrods found in sentence
+ */
+
+// let arr = ["hello words", "I love coding" , "DSA"];
+
+
+// let max = 0;
+
+
+// for(let i=0;i<arr.length;i++){
+//   let sen = arr[i];
+//   let sp = sen.split(" ");
+//   let len = sp.length;
+//   if(max<len){
+//      max = len
+//   }
+// }
+
+// console.log(max)
+
+
+
+
+
+
